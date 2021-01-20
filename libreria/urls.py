d14c3d5ecc8b4a826c3819c2ee55c22f35c64fb6
Loz-Aimone <71756464+Loz-Aimone@users.noreply.github.com>
2021-01-20 12:37:25 +0100

    messagge

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import home, Autore_ALDetailViewCB, Libro_ALListView
+from .views import Autore_ALDetailViewCB, Libro_ALListView
 app_name="libreria"
 urlpatterns = [
-    path("",home, name="homeview"),
-    #path("articoli/<int:pk>", articoloDetailView, name="articolo_detail")
     path("autori/<int:pk>", Autore_ALDetailViewCB.as_view(), name="autore_detail"),
     path("lista_libri/", Libro_ALListView.as_view(), name="lista_libri"),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import Libro_AL, Autore_AL
 from django.views.generic.detail import DetailView 
 from django.views.generic.list import ListView
+"""
 def home(request):
     libri = Libro.objects.all()
     autori = Autore.objects.all()
     context = {"libri": libri, "autori": autori}
     print(context)
     return render(request, "homepage.html", context)
-
-class LibroListView(ListView):
-    model = Libro
+"""
+class Libro_ALListView(ListView):
+    model = Libro_AL
     template_name = "lista_libri.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["libri"] = Libro.objects.all()
+        context["libri"] = Libro_AL.objects.all()
         return context
 
-class AutoreDetailViewCB(DetailView):
-    model = Autore
+class Autore_ALDetailViewCB(DetailView):
+    model = Autore_AL
     template_name = "autore_detail.html"
 
 
